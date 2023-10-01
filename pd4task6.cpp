50337df8d2fae1732cbb28dfb7cb5c5da8a88089
zaidgamer-1234 <144407789+zaidgamer-1234@users.noreply.github.com>
2023-10-01 23:47:02 +0500

    Add files via upload

---
+++ pd4task6.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void printH()
+{
+    cout<<"H"<<endl;
+}
+
+void printa()
+{
+    cout<<"a"<<endl;
+}
+
+
+void prints()
+{
+    cout<<"s"<<endl;
+}
+
+
+void printn()
+{
+    cout<<"n"<<endl;
+}
+
+main()
+{
+    printH();
+    printa();
+    prints();
+    prints();
+    printa();
+    printn();
+
+}
