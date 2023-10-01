0992cafa6d13b171edf94fc11da42b6dcab3ee0b
zaidgamer-1234 <144407789+zaidgamer-1234@users.noreply.github.com>
2023-10-01 23:45:56 +0500

    Add files via upload

---
+++ pd4task3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+
+void printMaze()
+{
+ cout << "#######################" << endl;
+ cout << "#                     #" << endl;
+ cout << "#                     #" << endl;
+ cout << "#                     #" << endl;
+ cout << "#                     #" << endl;
+ cout << "#                     #" << endl;
+ cout << "#                     #" << endl;
+ cout << "#                     #" << endl;
+ cout << "#######################" << endl;
+}
+
+void gotoxy(int x, int y)
+{
+ COORD coordinates;
+ coordinates.X = x;
+ coordinates.Y = y;
+ SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
+}
+
+main()
+{
+    system("cls");
+    printMaze();
+    gotoxy(4,3);
+    cout<<"P";
+    gotoxy(0 , 10);
+}
