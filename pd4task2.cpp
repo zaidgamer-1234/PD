deeb1099adfab0c1ca0ba3d72523d7cd04605a6d
zaidgamer-1234 <144407789+zaidgamer-1234@users.noreply.github.com>
2023-10-01 23:45:36 +0500

    Add files via upload

---
+++ pd4task2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void printMaze()
+{
+ cout << "#######################" << endl;
+ cout << "#                     #" << endl;
+ cout << "#                     #" << endl;
+ cout << "#                     #" << endl;
+ cout << "#                     #" << endl;
+ cout << "#                     #" << endl;
+ cout << "#                     #" << endl;
+ cout << "#                     #" << endl;
+ cout << "#######################" << endl;
+}
+
+main()
+{
+    system("cls");
+    printMaze();
+}
